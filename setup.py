76e663b6a2bb1f3d4fa79e581b3244f3d6319220
pedrosaclout <51131677+pedrosaclout@users.noreply.github.com>
2019-05-28 20:03:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 from setuptools import setup
 
 version = "2.0.1"
-url = "https://github.com/JoshuaRLi/pylibgen"
+url = "https://github.com/pedrosaclout/pylibgen-1"
 
 here = os.path.abspath(os.path.dirname(__file__))
 
